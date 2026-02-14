0dcbe8ec52999928254102f641dd7f68ba7a3e99
cristiandiaz300592 <cristiandiaz300592@gmail.com>
2026-02-14 01:12:14 -0300

    Update script_v4.js

--- script_v4.js
+++ script_v4.js
@@ -3,6 +3,7 @@ const contenedor = document.getElementById("calendario");
 // 🔗 URLs CSV
 const CSV_A = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSwAjG8iObcMS7Zwum05QM61k6on31_lCsxA4UFWx6nvTiA1BfA1_loV1Mc0N6mHAxEYXjO_ukKSgw/pub?gid=0&single=true&output=csv";
 const CSV_B = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSwAjG8iObcMS7Zwum05QM61k6on31_lCsxA4UFWx6nvTiA1BfA1_loV1Mc0N6mHAxEYXjO_ukKSgw/pub?gid=356969972&single=true&output=csv";
+const CSV_INT = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSwAjG8iObcMS7Zwum05QM61k6on31_lCsxA4UFWx6nvTiA1BfA1_loV1Mc0N6mHAxEYXjO_ukKSgw/pub?gid=2047754953&single=true&output=csv";
 
 // 📅 Fecha de hoy
 const hoy = new Date();
@@ -36,8 +37,10 @@ function cargarCSV(url, division) {
 
 Promise.all([
   cargarCSV(CSV_A, "Primera A"),
-  cargarCSV(CSV_B, "Primera B")
+  cargarCSV(CSV_B, "Primera B"),
+  cargarCSV(CSV_INT, "Internacional")
 ])
+
 .then(data => {
 
   const partidos = data.flat();
@@ -113,6 +116,7 @@ Promise.all([
   console.error(err);
   contenedor.innerHTML = "<p>Error cargando el calendario</p>";
 });
+
 
 
 
