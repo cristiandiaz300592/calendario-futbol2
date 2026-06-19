2f22134a70b26d920f9fbcd3cf48b10850ff59e0
cristiandiaz300592 <cristiandiaz300592@gmail.com>
2026-06-18 21:55:36 -0400

    Update script_v4.js

--- script_v4.js
+++ script_v4.js
@@ -5,6 +5,7 @@ const CSV_A = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSwAjG8iObcMS7Zw
 const CSV_B = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSwAjG8iObcMS7Zwum05QM61k6on31_lCsxA4UFWx6nvTiA1BfA1_loV1Mc0N6mHAxEYXjO_ukKSgw/pub?gid=356969972&single=true&output=csv";
 const CSV_INT = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSwAjG8iObcMS7Zwum05QM61k6on31_lCsxA4UFWx6nvTiA1BfA1_loV1Mc0N6mHAxEYXjO_ukKSgw/pub?gid=2047754953&single=true&output=csv";
 const CSV_NUEVO = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSwAjG8iObcMS7Zwum05QM61k6on31_lCsxA4UFWx6nvTiA1BfA1_loV1Mc0N6mHAxEYXjO_ukKSgw/pub?gid=1979663092&single=true&output=csv";
+const CSV_COPA_CHILE = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSwAjG8iObcMS7Zwum05QM61k6on31_lCsxA4UFWx6nvTiA1BfA1_loV1Mc0N6mHAxEYXjO_ukKSgw/pub?gid=302573942&single=true&output=csv";
 
 // 📅 Fecha de hoy
 const hoy = new Date();
